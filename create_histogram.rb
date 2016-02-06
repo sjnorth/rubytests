13e84f20d92c5bc1da898928570e9d2d40985879
SJ NORTH <samanthajmnorth@gmail.com>
2016-02-06 10:22:49 +0200

    new

--- create_histogram.rb
+++ create_histogram.rb
@@ -1,6 +1,6 @@
 #this is a simple histogram that takes user input and counts the number of times each word is used. it then outputs each key/value pair in reverse numerical order.
 puts "Please tell me what you want to say"
-text = gets.chomp
+text = gets.chomp      
 
 words = text.split(" ") #create an array by splitting string
 frequencies = Hash.new(0) #create new hash with default value zero
